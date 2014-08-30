0c658c35b85614c9eeb9e394e7eda65e25dffb40
JAYAVELU <jayaveluraj@users.noreply.github.com>
2014-08-30 19:00:52 +0530

    Initial Commit

---
+++ View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  MyApp
+//
+//  Created by Jayavelu on 8/30/14.
+//  Copyright (c) 2014 BhanuPrakash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIViewController
+
+@end
---
+++ View.m
@@ -0,0 +1,38 @@
+//
+//  View.m
+//  MyApp
+//
+//  Created by Jayavelu on 8/30/14.
+//  Copyright (c) 2014 BhanuPrakash. All rights reserved.
+//
+
+#import "View.h"
+
+@interface View ()
+
+@end
+
+@implementation View
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
